34af024d4682644078970a2aca58820f98409636
Julio César Méndez Marante <mendezmarantejuliocesar@gmail.com>
2024-05-13 15:05:17 +0200

    initialcommit
    Agragado los archivos

---
+++ access/js/abrircerrar.js
@@ -0,0 +1,15 @@
+
+const btnToggle = document.querySelector('.btn_toggle');
+
+btnToggle.addEventListener('click', function () {
+  document.getElementById('contenido').classList.toggle('active');
+})
+
+
+
+
+
+
+
+
+
